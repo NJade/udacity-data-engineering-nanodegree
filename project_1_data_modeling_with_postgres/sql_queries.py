083d643f7c0c32619d1bb94c5f6bf8ecb8cc74b8
Njade <njade412@gmail.com>
2020-08-09 09:55:25 +0900

    Add etl notebook (#1)

--- project_1_data_modeling_with_postgres/sql_queries.py
+++ project_1_data_modeling_with_postgres/sql_queries.py
@@ -9,46 +9,104 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""
-    CREATE TABLE IF NOT EXISTS songplays (songplay_id INT, start_time TIMESTAMP, user_id INT, level VARCHAR, song_id INT, session_id INT, location VARCHAR, user_agent VARCHAR, PRIMARY KEY (songplay_id))
+    CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL, \
+        start_time BIGINT, \
+        user_id INT, \
+        level VARCHAR, \
+        song_id INT, \
+        artist_id VARCHAR, \
+        session_id INT, \
+        location VARCHAR,\
+        user_agent VARCHAR, \
+        PRIMARY KEY (songplay_id))
 """)
 
 user_table_create = ("""
-    CREATE TABLE IF NOT EXISTS users (user_id INT, first_name VARCHAR, last_name VARCHAR, gender VARCHAR, level VARCHAR, PRIMARY KEY (user_id))
+    CREATE TABLE IF NOT EXISTS users (user_id INT, \
+        first_name VARCHAR, \
+        last_name VARCHAR, \
+        gender VARCHAR, \
+        level VARCHAR, \
+        PRIMARY KEY (user_id))
 """)
 
 song_table_create = ("""
-    CREATE TABLE IF NOT EXISTS songs (song_id INT, title VARCHAR, artist_id VARCHAR, year INT, duration NUMERIC, PRIMARY KEY (song_id))
+    CREATE TABLE IF NOT EXISTS songs (song_id VARCHAR, \
+        title VARCHAR, \
+        artist_id VARCHAR, \
+        year INT, \
+        duration NUMERIC, \
+        PRIMARY KEY (song_id))
 """)
 
 artist_table_create = ("""
-    CREATE TABLE IF NOT EXISTS artists (artist_id VARCHAR, name VARCHAR, location VARCHAR, latitude NUMERIC, longitude NUMERIC, PRIMARY KEY (artist_id))
+    CREATE TABLE IF NOT EXISTS artists (artist_id VARCHAR, \
+        name VARCHAR, \
+        location VARCHAR, \
+        latitude NUMERIC, \
+        longitude NUMERIC, \
+        PRIMARY KEY (artist_id))
 """)
 
 time_table_create = ("""
-    CREATE TABLE IF NOT EXISTS time (start_time TIMESTAMP, hour INT, day INT, week INT, year INT, weekday INT, PRIMARY KEY (start_time))
+    CREATE TABLE IF NOT EXISTS time (start_time BIGINT, \
+        hour INT, \
+        day INT, \
+        week INT, \
+        month INT, \
+        year INT, \
+        weekday INT, \
+        PRIMARY KEY (start_time))
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
+    VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+    INSERT INTO users(user_id, first_name, last_name, gender, level) \
+    VALUES(%s, %s, %s, %s, %s) \
+    ON CONFLICT(user_id) \
+    DO UPDATE \
+        SET level = EXCLUDED.level
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs(song_id, title, artist_id, year, duration) \
+    VALUES(%s, %s, %s, %s, %s) \
+    ON CONFLICT(song_id) \
+    DO NOTHING
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists(artist_id, name, location, latitude, longitude) \
+    VALUES(%s, %s, %s, %s, %s) \
+    ON CONFLICT(artist_id) \
+    DO NOTHING
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time(start_time, hour, day, week, month, year, weekday) \
+    VALUES(%s, %s, %s, %s, %s, %s, %s) \
+    ON CONFLICT(start_time) \
+    DO NOTHING
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT song_id, songs.artist_id \
+    FROM songs \
+    JOIN artists \
+    ON songs.artist_id = artists.artist_id \
+    WHERE \
+        title = %s AND \
+        name = %s AND \
+        duration = %s
 """)
 
 # QUERY LISTS
